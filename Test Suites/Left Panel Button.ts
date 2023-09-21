6812e72db0e4a3ceb04be460f0cc114173c93637
faked <faked@Fakedoor>
2023-09-21 10:06:24 +0100

    Left Panel Test Suite

---
+++ Test Suites/Left Panel Button.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Left Panel Button</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>209a52e2-f038-4611-8134-719ba2ff2f2d</testSuiteGuid>
+   <testCaseLink>
+      <guid>df55dd2c-bd44-4b79-ae7b-90cb8eeb78bd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Left Panel Buttons/Left Panel Buttons</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
